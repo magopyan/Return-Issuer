d59f5d49adf4461ba01882a92649173af388d3d1
Mihael Agopyan <mihael.b.agopyan@gmail.com>
2021-10-01 15:34:36 +0300

    Made changes to make a .jar file runnable
    - JavaFX version changed from 13 to 17.0.0.1 so that I can download the SDK if I need it, since the one for v13 is not available anywhere on the internet (using just "17" made Maven download a version that doesn't work, has to be "17.0.0.1"
    - Added plugins to pom.xml
    - Maven Shade allows including the external libraries in the .jar file which is the only way to run JavaFX
    - Added new class App which has a main that just executes Main class' main method. This new App class is the <mainClass> for the Maven Shade plugin since the .jar won't run if the main class is extending something (in Main's case extends Application)

--- src/main/java/enums/AutoAcceptMode.java
+++ src/main/java/enums/AutoAcceptMode.java
@@ -4,15 +4,5 @@ public enum AutoAcceptMode {
     on,
     off;
 
-    /*@Override
-    public String toString() {
 
-        switch(this) {
-            case ON:
-                return "on";
-            case OFF:
-                return "off";
-            default: throw new IllegalArgumentException();
-        }
-    }*/
 }
